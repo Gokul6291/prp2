ced35686a950a5d719479aa47167facbb46dc1da
Gokul6291 <60608177+Gokul6291@users.noreply.github.com>
2020-06-23 20:57:00 +0530

    Add files via upload

---
+++ src/Arrayt4.java
@@ -0,0 +1,12 @@
+
+
+public class Arrayt4 {
+    public static void main(String args[]){
+        int numbers[] = {65, 120, 98, 75, 115};
+        String str =null;
+        for(int i: numbers){
+            str = Character.toString((char)i);
+            System.out.print(str);
+        }
+    }
+}
