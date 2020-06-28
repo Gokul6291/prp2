c188d1e65df668460b48f34255e38182352c4a0c
Gokul6291 <60608177+Gokul6291@users.noreply.github.com>
2020-06-28 21:28:03 +0530

    Add files via upload

---
+++ src/Even.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class Even {
+	static int even(int num) {
+		if(num%2==0) {
+			return 2;
+		}
+		else {
+			return 1;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int num=s.nextInt();
+		System.out.println(even(num));
+	}
+
+}
